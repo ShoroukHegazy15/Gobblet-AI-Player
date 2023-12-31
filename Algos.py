c1b1057602c15a9239e26d09581c4939db0d37bf
Monica 1900199 <104925298+MonicaMickie@users.noreply.github.com>
2023-12-31 20:03:53 +0200

    Update Algos.py

--- Algos.py
+++ Algos.py
@@ -31,3 +31,31 @@ class Algos:
                     bestMove = move
 
         return bestScore, bestMove
+
+  def alphaBeta(self, board, maxDepth, currentDepth, alpha, beta):
+        # Check if we’re done recursing
+        if board.game_is_over() or currentDepth == maxDepth:
+            # check if player is same as current_player attribute of board
+            player = board.currentPlayer()
+            return Evaluate.evaluate(board.board_state), None
+
+        # Otherwise get values from below
+        bestMove = None
+        bestScore = -INFINITY
+    
+        # Go through each move
+        for move in View.get_valid_moves_for_black_pieces():
+            newBoard = board.make_move(move, player)
+            # Recurse
+            recursedScore, currentMove = self.alphaBeta(newBoard, maxDepth, currentDepth + 1, -beta, -max(alpha, bestScore))
+            currentScore = -recursedScore
+    
+            # Update the best score
+            if currentScore > bestScore:
+                bestScore = currentScore
+                bestMove = move
+    
+            # If we’re outside the bounds, then prune: exit immediately
+            if bestScore >= beta:
+                return bestScore, bestMove
+        return bestScore, bestMove
