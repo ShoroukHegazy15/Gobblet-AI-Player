98ae503e73d02ade72b716060c4f600e12a39f62
mariamsamir037 <104376789+mariamsamir037@users.noreply.github.com>
2024-01-16 21:29:33 +0200

    fix alphabeta alg and add pruning condition

--- Algo_HVC.py
+++ Algo_HVC.py
@@ -46,7 +46,6 @@ class Algos:
                     # Append the dragged_piece to the list at new_position
                     ViewHVC.pieces[new_position].append(chosen_piece)
                     
-            #if newBoard is not None:
                 nowPlayer = 3 - player  # Switch players
                 currentScore, _ = self.minimax(ViewHVC, newBoard, nowPlayer, maxDepth, currentDepth + 1)
 
@@ -59,110 +58,79 @@ class Algos:
                         bestScore = currentScore
                         bestMove = move
                 board.undo_last_move(player)    #4elt mn boardstate
-                #3yzeen n update el pieces kman
-                #if new_position in ViewHVC.pieces :
-                    # Remove the dragged_piece from the list at old_position
+                
                 ViewHVC.pieces[new_position].remove(chosen_piece)
-                #if old_position in ViewHVC.pieces and chosen_piece in ViewHVC.pieces[old_position]:
-                    # Append the dragged_piece to the list at new_position
                 ViewHVC.pieces[old_position].append(chosen_piece)
                 
-                #if old_position in ViewHVC.pieces and ViewHVC.pieces[old_position]:  # Check if the list is not empty
-                #moved_piece = ViewHVC.pieces[move.old_position].pop()
-                #ViewHVC.pieces[move.start_position].append(move.piece_size)
-
-        #print("bestscore",{bestScore})
         return bestScore, bestMove
 
     def getBestMoveAlphaBeta(self, ViewHVC, board, player, maxDepth):
-            # if(player==2 and self.view.board.currentPlayer()==1):
-            #     self.view.board.switchPlayer()
-            # elif(player==1 and self.view.board.currentPlayer()==2):
-            #     self.view.board.switchPlayer()
-            # self.view.board=board
-            # self.view.pieces=pieces
-            # Get best move for alpha beta algo
-            score, move = self.alphaBeta(ViewHVC, board, player, maxDepth, 0, -INFINITY, INFINITY)
-            self.best_move = move
-            # return move
-            return move
-    
-    def getBestMoveAlphaBetaID(self, ViewHVC, board, player, maxDepth):
-        # if(player==2 and self.view.board.currentPlayer()==1):
-        #     self.view.board.switchPlayer()
-        #     self.view.board.switchPlayer()
-        # elif(player==1 and self.view.board.currentPlayer()==2):
-        # self.view.board=board
-        # self.view.pieces=pieces
-        # Get best move for Alpha Beta Iterative Deepening algo
-        bestMove = None
-        for depth in range(1, maxDepth + 1):
-            score, move = self.alphaBeta(ViewHVC, board, player, depth, -INFINITY, INFINITY)
-            bestMove = move
-            self.best_move = move
-        return bestMove
-    
+    # Get best move for alpha beta algo
+        _, move = self.alphaBeta(ViewHVC, board, player, maxDepth, 0, -INFINITY, INFINITY)
+        self.best_move = move
+        return move
+
+        
     def alphaBeta(self, ViewHVC, board, player, maxDepth, currentDepth, alpha, beta):
-        self.i +=1
-        print("i=", self.i)
-        print("\n")
         # Check if we’re done recursing
         if ViewHVC.game_is_over() or currentDepth == maxDepth:
-            # check if player is same as current_player attribute of board
             return Evaluation.evaluate(ViewHVC, board), None
     
         # Otherwise get values from below
         bestMove = None
-        bestScore = -INFINITY
+        bestScore = -INFINITY if player == 2 else INFINITY  # Initialize based on player's turn
     
-        # Go through each move
-        # if(player==1):
-        #     playerColor="white"
-        # elif(player==2):
-        #     playerColor="black"
         valid_moves = ViewHVC.get_valid_moves_for_pieces(self.player_colors[player])
-    
         for i in range(len(valid_moves)):
             #hna al valid moves btt3ml azai 
             move=valid_moves[i]
+            if move and move.start_position is not None and move.end_position is not None:
+                old_position = move.start_position
+                new_position = move.end_position
+                
+            # Update the position of the chosen piece as if it's being dragged by the computer
+                chosen_piece = ViewHVC.get_piece_at_position(old_position)
+                
             newBoard = board.make_move(move, player)
-    
             if newBoard is not None:
+            # If the move was successful, update the current board
+                if old_position in ViewHVC.pieces and chosen_piece in ViewHVC.pieces[old_position]:
+                    # Remove the dragged_piece from the list at old_position
+                    ViewHVC.pieces[old_position].remove(chosen_piece)
+                if new_position in ViewHVC.pieces :
+                    # Append the dragged_piece to the list at new_position
+                    ViewHVC.pieces[new_position].append(chosen_piece)
+                
                 nowPlayer = 3 - player  # Switch players
-                currentScore, _ = self.alphaBeta(ViewHVC, newBoard, nowPlayer, maxDepth, currentDepth + 1, -beta, -max(alpha, bestScore))
-                board.undo_last_move()
-    
-            # if move.start_position in self.view.pieces and self.view.pieces[move.start_position]:  # Check if the list is not empty
-            #     moved_piece = self.view.pieces[move.start_position].pop()
-            #     self.view.pieces[move.end_position].append(moved_piece)
-            
-            # Recurse
-            # recursedScore, _ = self.alphaBeta(newBoard, nowPlayer, maxDepth, currentDepth + 1, -beta, -max(alpha, bestScore))
-            # currentScore = -recursedScore
-    
-            # Update the best score
-                if board.currentPlayer() == player:
+                currentScore, _ = self.alphaBeta(ViewHVC, newBoard, nowPlayer, maxDepth, currentDepth + 1, alpha, beta)
+                
+                """ # Update the best score
+                if board.currentPlayer() == player:    #maxi  black 2 AI
                     if currentScore > bestScore:
                         bestScore = currentScore
                         bestMove = move
                     else:
-                        if currentScore < bestScore:
+                        if currentScore < bestScore:   #mini white 1 ana
                             bestScore = currentScore
-                            bestMove = move
-    
-            # Update alpha for pruning
-            # alpha = max(alpha, bestScore)
-    
-            # If we’re outside the bounds, then prune: exit immediately
-            # if bestScore >= beta:
-            #     return bestScore, bestMove
-            
-            # if move.end_position in self.view.pieces and self.view.pieces[move.end_position]:  # Check if the list is not empty
-            #     undoedPiece = self.view.pieces[move.end_position].pop()
-            #     self.view.pieces[move.start_position].append(undoedPiece)
+                            bestMove = move """
+                 # Update the best score
+                if player == 2:  # Maximizing player (black AI)
+                    bestScore = max(bestScore, currentScore)
+                    if currentScore == bestScore:
+                        bestMove = move
+                    alpha = max(alpha, bestScore)  # Update alpha for pruning
+                else:  # Minimizing player (white human)
+                    bestScore = min(bestScore, currentScore)
+                    if currentScore == bestScore:
+                        bestMove = move
+                    beta = min(beta, bestScore)  # Update beta for pruning
                 
-            # undoedMove=Move(move.end_position,move.start_position,move.piece_size)
-            # self.view.board.undo_moves(undoedMove,player)
-        print("bestscore",{bestScore})
+                board.undo_last_move(player)
+                ViewHVC.pieces[new_position].remove(chosen_piece)
+                ViewHVC.pieces[old_position].append(chosen_piece)
+                
+                # Pruning condition
+                if beta <= alpha:
+                    break
+
         return bestScore, bestMove
-        
